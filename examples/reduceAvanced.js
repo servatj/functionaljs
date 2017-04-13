8f883400b53bcda422aa969b6bf7dc8107956c38
Josep <servatj@Joseps-MacBook-Pro.local>
2017-04-13 23:32:08 +0200

    Advanced reduce example

--- examples/reduceAvanced.js
+++ examples/reduceAvanced.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-const rawData = `
-Magician  potion1 80 4
-Magician  potion2 60 2
-Magician  potion3 50 1
+const fileName = './data/sample.dat'
+const rawData = `Magician potion1 80 4
+Magician potion2 60 2
+Magician potion3 50 1
 knight weapon1 60 3
 knight weapon2 60 3
 knight weapon3 60 3
@@ -16,11 +16,31 @@ const createFile = (file, contents, format) => {
        resolve(contents);
      });
   })
-}
+} 
 
-createFile('./data/sample.dat', rawData, 'utf8')
+const output = fs.readFileSync(fileName, 'utf8')
+  .trim() 
+  .split('\n')
+  .map((line) => line.split(' '))
+  .reduce((heroes,line) => {
+     heroes[line[0]] = heroes[line[0]] || []
+     heroes[line[0]].push({
+       name:     line[1],
+       item:     line[2],
+       damage:   line[3],
+       quantity: line[4]
+      })
+      return heroes
+   } , {})
+
+
+// promise call starting point 
+createFile(fileName, rawData, 'utf8') 
   .then((contents) => {
     console.log('Ok Success' + contents)
-  }).catch((err) => {
+  }).then((err) => {
+  	console.log(JSON.stringify(output, null, 2))
+  } 
+  ).catch((err) => {
     console.log('Error')  
 })
